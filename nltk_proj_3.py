79173505f0f3a3618dd1d6972183e2fba7bff636
Tomek <tomekradecki@gmail.com>
2021-08-07 22:23:19 +0200

    Chinking

--- nltk_proj_3.py
+++ nltk_proj_3.py
@@ -16,7 +16,9 @@ def process_content():
             words = nltk.word_tokenize(i)
             tagged = nltk.pos_tag(words)
 
-            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?} """
+            # chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?} """
+            chunkGram = r"""Chunk: {<.*>+} 
+                                }<VB.?|IN|DT|TO+>{"""
 
             chunkParser = nltk.RegexpParser(chunkGram)
             chunked = chunkParser.parse(tagged)
