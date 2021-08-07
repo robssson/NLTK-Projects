e12d554da935488cdbdfa9f2ab5fc28a55c191d9
Tomek <tomekradecki@gmail.com>
2021-08-07 10:02:35 +0200

    Remove stop words from example sentence

--- nltk_proj_1.py
+++ nltk_proj_1.py
@@ -1,4 +1,5 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 
 # nltk.download()  --> I would like to recommend you to install and download 'all'
 
@@ -9,5 +10,13 @@ print(f"Length of sent tokenize: {len(sent_tokenize(example_text))}")
 print(word_tokenize(example_text))
 print(f"Length of word tokenize: {len(word_tokenize(example_text))}")
 
-for word in word_tokenize(example_text):
-    print(word)
+stop_words = set(stopwords.words("english"))
+
+words = word_tokenize(example_text)
+
+filtered_text = []
+
+filtered_text = [word for word in words if word not in stop_words]
+print(filtered_text)
+print(f"Length of word tokenize after filtering (stop_words): {len(filtered_text)}")
+
